f5c3dd363f94f939ad42872dd3ba689756d88ffb
André Campanhã <andre.campanha42@gmail.com>
2018-12-14 10:33:52 +0000

    Updated prob6 PL10

--- pl10/prob6.c
+++ pl10/prob6.c
@@ -2,16 +2,6 @@
 
 char converte(char c, int shift)
 {
-    /*if(c + shift > 'z')
-    {
-        shift -= 'z' - c + 1;
-        c = 'a';
-    }
-    else if(c + shift < 'a')
-    {
-        shift -= c - 'a' - 1;
-        c = 'z';
-    }*/
     int r = (c + shift - 'a') % 26;
     return (r < 0 ? 'z' + r + 1 : r + 'a');
 }
